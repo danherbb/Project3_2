3814870ded41ca4efce613bf9cceabf72938da6a
Krishp25 <117307626+Krishp25@users.noreply.github.com>
2025-04-21 13:22:02 -0400

    Update huffman.h

--- src/huffman.h
+++ src/huffman.h
@@ -2,7 +2,13 @@
 #define PROJECT3_2_HUFFMAN_H
 
 #include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
+vector<int> huffmanEncodeToNumbers(const string& text);
+string huffmanEncodeToString(const string& text);
+double huffmanCompRatio(const string& original, const string& encoded);
+double huffmanCompRatio(const string& original, const vector<int>& encoded);
 
 #endif //PROJECT3_2_HUFFMAN_H
